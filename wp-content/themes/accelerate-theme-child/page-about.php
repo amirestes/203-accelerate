f60fd4306cdcdff3568d44ed77ba4f6bf98bb054
amirestes <amirestes@gmail.com>
2021-01-24 19:58:22 -0600

    fixed main div on page-about

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -6,36 +6,36 @@
 *
 *This is the template that displays about pages by default. 
 **/
+
 get_header(); ?>
-    <div id="primary" class="hero-content about">
+
+<div id="primary" class="hero-content about">
         <div class="main-content" role="main">
             <p class="about-image">Accerlerate is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</p>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php the_content(); ?>
-            <?php endwhile; ?>
         </div>
-    </div>
-    <!-- services -->
-    <section class="about-services">
-        <div class="about-subheading"
-            <?php while ( have_posts() ) : the_post();
-                $subheading = get_field('subheading');
-                $description = get_field('description');
-                $service_icon = get_field('service_icon');
-                $size = "small" 
-            ?>
-                 <div class="services">
-                    <h3>Our Services</h3>
-                    <p> We take pride in our clients and the content we create for them. Here's a brief overview of our offered services.</p>
-                </div>
-        </div>
-         <div class="about_services">
-                <h3><?php echo $subheading; ?></h3>
-                <p><?php echo $desccription; ?></p>
-                <img src="<?php echo $service_icon; ?>" />
-                
-                <?php the_content(); ?>
-            </div>
-        <?php endwhile; //end of the loop. ?>
-   </section>
+</div>
+
+<div class="our-services">
+                <?php while ( have_posts() ) : the_post(); 
+                    $subheading = get_field('subheading');
+                    $description = get_field('description');
+                    $icon = get_field('service_icon');
+                    $size = "full";
+                ?>
+                <section class="services">
+                    <div class="subheading">
+                        <div class="services">
+                            <h3>Our Services</h3>
+                            <p> We take pride in our clients and the content we create for them. Here's a brief overview of our offered services.</p>
+                        </div>
+                        <div class="about_services">
+                            <h3><?php echo $subheading; ?></h3>
+                            <p><?php echo $description; ?></p>
+                            <img src="<?php echo $service_icon; ?>" />
+                        </div>
+                        <?php the_content(); ?>
+                    <?php endwhile; ?>
+                    </div>
+            </div>   
+                </section>
 <?php get_footer(); ?>
