febfdc2e3cc34ef45bd83012e5762cdccc64f20f
amirestes <amirestes@gmail.com>
2021-01-28 18:51:55 -0600

    adjusted padding on services
    git status

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -10,52 +10,50 @@
 get_header(); ?>
 
 <div id="primary" class="hero-content about">
-        <div class="main-content" role="main">
+        <div class="about-content" role="main">
             <p class="about-image">Accerlerate is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</p>
             <?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
         </div>
 </div>
-<!--services --> 
-<div class="our-services">
-<h4>Our Services</h4>
-            <p id="our-services"> We take pride in our clients and the content we create for them. Here's a brief overview of our offered services. </p>
-                <?php while ( have_posts() ) : the_post(); 
-                    $subheading = get_field("subheading");
-                    $text = get_field("text");
-                    $icon = get_field("service_icon");
-                    $size = "full";
-                ?>
-                     <h4><?php echo $subheading ?></h4>
-			         <p><?php echo $text ?></p>
-				<?php endwhile; //end of the loop ?>         
-</div>
+<!--services -->
 
-<section class="featured-work">
+<section class="about-content">
+	<div class="our-services">
+	<h4>Our Services</h4>
+			<p id="our-services"> We take pride in our clients and the content we create for them. Here's a brief overview of our offered services. </p>
+
+	<section class="featured-work">
 		<div class="site-content">
-
-			
 			<ul class="homepage-featured-work">
-			<?php query_posts('posts_per_page=4&post_type=services'); ?>
+				<?php query_posts('posts_per_page=4&post_type=services'); ?>
 					<?php while ( have_posts() ) : the_post();
 					$icon = get_field("service_icon");
-                    $size = "small";
-                    $text = get_field("text");
-			?>
+                    $size = "large";
+					$text = get_field("text");
+					$subheading = get_field("subheading");
+				?>
            
-			<li class="individual-featured-work">
-				<figure>
-				<?php echo wp_get_attachment_image($icon, $size); ?>
-				</figure>
-					<h3><a href="<?php the_permalink(); ?>" class="read-more=link">
-					<?php the_title(); ?></a></h3>
-					<p><?php echo $text ?></p>
-					
+			<li class="about-services">
+					<figure class="icon"><?php echo  wp_get_attachment_image($icon, $size) ?></figure>
+					<div class="text">
+						<h3 class="subheading"><?php echo $subheading ?></h3>
+						<p class="text"><?php echo $text ?></p>
+					</div>
 			</li>
 				<?php endwhile; ?>
 			<?php wp_reset_query(); ?>
+
 			</ul>
 		</div>
 	</section>
+	<section class="about-cta">
+			<div class="contact-us-button">
+		    	<h2>Interested in working with us?</h2>
+			  	<a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+			</div>
+</section>
+</div>
+</section>
 <?php get_footer(); ?>
